55b8ef8abb8b10823d845fb980635990eeed38ac
hungdoan-wq <hung.doan@katalon.com>
2026-03-23 11:28:05 +0700

    initial sync from KS

---
+++ Scripts/Google/TC_Verify_ImageOrText_Above_SearchBox/Script1774239375072.groovy
@@ -0,0 +1,46 @@
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+String url = 'https://www.google.com/'
+int timeout = 15
+
+TestObject byXpath(String xpath) {
+    TestObject to = new TestObject()
+    to.addProperty('xpath', ConditionType.EQUALS, xpath)
+    return to
+}
+
+try {
+    WebUI.openBrowser('')
+    WebUI.maximizeWindow()
+    WebUI.navigateToUrl(url)
+
+    // 1) Wait for the search box
+    TestObject searchBox = byXpath("//textarea[@name='q'] | //input[@name='q']")
+    WebUI.verifyElementPresent(searchBox, timeout)
+    WebUI.verifyElementVisible(searchBox)
+
+    // 2) Verify there is either an image OR non-empty text above the search box (layout-wise)
+    // Use the nearest preceding element that is an <img> or has non-empty visible text.
+    TestObject aboveSearch_hasImageOrText = byXpath('''
+        (
+          //textarea[@name='q'] | //input[@name='q']
+        )
+        /preceding::*[
+            (self::img)
+            or
+            (not(self::script) and not(self::style) and normalize-space(.) != '')
+        ][1]
+    ''')
+
+    boolean found = WebUI.verifyElementPresent(aboveSearch_hasImageOrText, timeout, FailureHandling.OPTIONAL)
+    WebUI.verifyEqual(found, true)
+
+    KeywordUtil.logInfo('Verified: there is an image or non-empty text above the search box.')
+
+} finally {
+    WebUI.closeBrowser()
+}
