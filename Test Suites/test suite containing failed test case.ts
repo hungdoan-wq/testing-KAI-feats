f5f24d6c574f7f9dd84870727d93032e73b7ac48
hungdoan-wq <hung.doan@katalon.com>
2026-03-23 12:56:49 +0700

    test suite

---
+++ Test Suites/test suite containing failed test case.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite containing failed test case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b350332f-b9ae-4a9a-b49b-eb481f146c56</testSuiteGuid>
+   <testCaseLink>
+      <guid>19bc3d44-5eb6-4f54-bbfb-6e1733e6a421</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Google/TC_Verify_ImageOrText_Above_SearchBox</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
