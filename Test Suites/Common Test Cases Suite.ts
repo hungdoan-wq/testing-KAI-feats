a630ef34f86c65cd732568bb06a3b0e338cf0f13
hung.doan@katalon.com <katalon_g3_dummy_username>
2026-04-07 03:35:18 +0000

    Create "Common Test Cases Suite" test suite

---
+++ Test Suites/Common Test Cases Suite.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>Common Test Cases Suite</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>e71fb78f-5fc3-44bf-bf8a-f361a94fe502</testSuiteGuid>
+    <testCaseLink>
+        <guid>47031789-a2f4-4415-acfa-30a56646d011</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Common Test Cases/Login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
